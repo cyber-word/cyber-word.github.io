547e0d7987caf061a618a9d866f771dc8a76caa9
cyber-word <67416400+cyber-word@users.noreply.github.com>
2020-06-25 19:11:38 +0800

    s

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>DY的网站</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/templatemo-style.css">
+<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<div class="site-bg"></div>
+<div class="site-bg-overlay"></div>
+
+<!-- TOP HEADER -->
+<div class="top-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<p class="phone-info">Call me qq: <a href="#">3513805092</a></p>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="social-icons">
+					<ul>
+						<li><a href="#" class="fa fa-facebook"></a></li>
+						<li><a href="#" class="fa fa-twitter"></a></li>
+						<li><a href="#" class="fa fa-linkedin"></a></li>
+						<li><a href="#" class="fa fa-dribbble"></a></li>
+						<li><a href="#" class="fa fa-rss"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> <!-- .top-header -->
+
+
+<div class="visible-xs visible-sm responsive-menu">
+	<a href="#" class="toggle-menu">
+		<i class="fa fa-bars"></i> Show Menu
+	</a>
+	<div class="show-menu">
+		<ul class="main-menu">
+			<li>
+				<a class="show-1 active homebutton" href="#"><i class="fa fa-home"></i>主页</a>
+			</li>
+			<li>
+				<a class="show-2 aboutbutton" href="#"><i class="fa fa-user"></i>关于我</a>
+			</li>
+			<li>
+				<a class="show-3 projectbutton" href="#"><i class="fa fa-camera"></i>展示</a>
+			</li>
+			<li>
+				<a class="show-5 contactbutton" href="#"><i class="fa fa-envelope"></i>联系</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="container" id="page-content">
+	<div class="row">
+
+
+		
+		<div class="col-md-9 col-sm-12 content-holder">
+			<!-- CONTENT -->
+			<div id="menu-container">
+				
+				<div class="logo-holder logo-top-margin">
+					<a href="#" class="site-brand"><img src="images/logo.png" height="50" width="50" alt=""></a>
+				</div>
+				
+				
+				<div id="menu-1" class="homepage home-section text-center">
+					<div class="welcome-text">
+						<h2>记录生活 ，<strong>留存回忆</strong></h2>
+						<p> <span>我从夜里醒来，望向窗外</span><span>在这楼房环绕的城市里</span>独自徘徊，我从夜里醒来，从梦里逃出来，只有风吹动的声音，全世界只有我自己清醒。</p>
+						<form action="#" method="get" class="subscribe-form">
+							<div class="row">
+								<fieldset class="col-md-offset-2 col-md-6">
+									<input name="subscribe-email" type="email" class="email" id="subscribe-email" placeholder="请输入你的邮箱...">
+								</fieldset>
+								<fieldset class="col-md-4 button-holder">
+									<input name="submit" type="submit" class="button default" id="submit" value="Submit">
+								</fieldset>
+							</div>
+							<p class="subscribe-text">或许我本该属于这大海，从来不属于如影随形的一种存在</p>
+						</form>
+					</div>
+				</div>
+
+				<div id="menu-2" class="content about-section">
+					<div class="row">
+						<div class="col-md-8 col-sm-8">
+							<div class="box-content profile">
+								<h3 class="widget-title">关于我最近听的歌</h3>
+								<div class="profile-thumb">
+									<img src="images/8.jpg" alt="">
+								</div>
+								<div class="profile-content">
+									<h5 class="profile-name">鲸鱼</h5>
+									<span class="profile-role">尚士达</span>
+									<p>挺好听的还，<a rel="nofollow" href="#">https://music.163.com/#/song?id=1362281761a> 先就这样。<br><br>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="box-content">
+								<h3 class="widget-title">Background</h3>
+								<p> 994997</p>
+								<div class="about-social">
+									<ul>
+										<li><a href="#" class="fa fa-facebook"></a></li>
+										<li><a href="#" class="fa fa-twitter"></a></li>
+										<li><a href="#" class="fa fa-linkedin"></a></li>
+										<li><a href="#" class="fa fa-dribbble"></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-5 col-sm-5">
+							<div class="box-content">
+								<h3 class="widget-title">Just For Fun</h3>
+								<div class="project-item">
+									<img src="images/7.jpg" alt="">
+									<div class="project-hover">
+										<h4>hhh</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7 col-sm-7">
+							<div class="box-content">
+								<h3 class="widget-title">My Technical Skills</h3>
+								<ul class="progess-bars">
+									<li>
+										<span>HTML CSS 10%</span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%;"></div>
+										</div>
+									</li>
+									<li>
+										<span>PHOTOSHOP 5%</span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100" style="width: 5%;"></div>
+										</div>
+									</li>
+									<li>
+										<span>WORDPRESS 0%</span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+										</div>
+									</li>
+									<li>
+										<span>PHP mySQL 10%</span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="menu-3" class="content gallery-section">
+					<div class="box-content">
+						<h3 class="widget-title">好看的图片</h3>
+						<div class="row">
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/1.jpg" alt="">
+									<div class="project-hover">
+										<h4>还行</h4>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/2.jpg" alt="">
+									<div class="project-hover">
+										<h4>不错</h4>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/3.jpg" alt="">
+									<div class="project-hover">
+										<h4>挺好看</h4>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/4.jpg" alt="">
+									<div class="project-hover">
+										<h4>可以</h4>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/5.jpg" alt="">
+									<div class="project-hover">
+										<h4>还行</h4>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="project-item">
+									<img src="images/6.jpg" alt="">
+									<div class="project-hover">
+										<h4>也还行</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="project-pages">
+							<ul>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">...</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div id="menu-4" class="content contact-section">
+					<div class="row">
+						<div class="col-md-8 col-sm-8">
+							<div class="box-content">
+								<h3 class="widget-title">联系我</h3>
+								<form class="contact-form">
+									<fieldset>
+										<input type="text" class="name" id="name" placeholder="Name...">
+									</fieldset> 
+									<fieldset>
+										<input type="email" class="email" id="email" placeholder="Email...">
+									</fieldset> 
+									<fieldset>
+										<input type="text" class="subject" id="subject" placeholder="Subject...">
+									</fieldset>
+									<fieldset>
+										<textarea name="message" id="message" cols="30" rows="4" placeholder="Message.."></textarea>
+									</fieldset>
+									<fieldset>
+										<input type="submit" class="button" id="button" value="Send Message">
+									</fieldset>
+								</form>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="box-content">
+								<h3 class="widget-title">Stay In Touch</h3>
+								<p>看到了，就是朋友了 <br><br>
+								我们走在这条孤独的路上到底要多久，从始至终 </p>
+								<div class="about-social">
+									<ul>
+										<li><a href="#" class="fa fa-facebook"></a></li>
+										<li><a href="#" class="fa fa-twitter"></a></li>
+										<li><a href="#" class="fa fa-linkedin"></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+
+		<div class="col-md-3 hidden-sm">
+			
+			<nav id="nav" class="main-navigation hidden-xs hidden-sm">
+				<ul class="main-menu">
+					<li>
+						<a class="show-1 active homebutton" href="#"><i class="fa fa-home"></i>主页</a>
+					</li>
+					<li>
+						<a class="show-2 aboutbutton" href="#"><i class="fa fa-user"></i>关于我</a>
+					</li>
+					<li>
+						<a class="show-3 projectbutton" href="#"><i class="fa fa-camera"></i>展示</a>
+					</li>
+					<li>
+						<a class="show-5 contactbutton" href="#"><i class="fa fa-envelope"></i>联系</a>
+					</li>
+				</ul>
+			</nav>
+
+		</div>
+	</div>
+</div>
+
+<!-- SITE-FOOTER -->
+<div class="site-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+			</div>
+		</div>
+	</div>
+</div> <!-- .site-footer -->
+
+<script src="js/vendor/jquery-1.10.2.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<!-- templatemo 439 rectangle -->
+</body>
+</html>
